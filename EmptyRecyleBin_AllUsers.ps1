1adee97ee131809dc3bd8155e7380a47567d59e6
amcanna1ly <44170799+amcanna1ly@users.noreply.github.com>
2024-04-15 12:23:33 -0500

    Update EmptyRecyleBin_AllUsers.ps1
    Updated Powershell file to run in Administrative mode automatically.

--- EmptyRecyleBin_AllUsers.ps1
+++ EmptyRecyleBin_AllUsers.ps1
@@ -1,4 +1,25 @@
-﻿$disks = Get-WmiObject Win32_LogicalDisk -Filter "DriveType=3"
+# Sets Powershell to run in Administrative Mode
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+'running with full privileges'
+####################################################
+
+#Main Script
+$disks = Get-WmiObject Win32_LogicalDisk -Filter "DriveType=3"
 
 foreach ($disk in $disks)
 {
