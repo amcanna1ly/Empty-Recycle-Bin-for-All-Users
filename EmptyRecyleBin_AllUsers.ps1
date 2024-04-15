e18f4bd43afe67c88013f67498b18a5688c12107
amcanna1ly <44170799+amcanna1ly@users.noreply.github.com>
2024-04-15 12:18:17 -0500

    Add files via upload

---
+++ EmptyRecyleBin_AllUsers.ps1
@@ -0,0 +1,11 @@
+﻿$disks = Get-WmiObject Win32_LogicalDisk -Filter "DriveType=3"
+
+foreach ($disk in $disks)
+{
+                  $rbpathnew = "$($disk.DeviceID)\`$Recycle.Bin"
+                  
+                  if (Test-Path $rbpathnew)
+                  {
+                                      cmd /c "rd /s /q $rbpathnew"
+                  }
+}
